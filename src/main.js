d953970f36a2afd11d5c42874012c319fcc06799
Levhita <142937923+levhitaIncode@users.noreply.github.com>
2025-05-13 16:47:20 -0600

    Updated to new guide (#7)
    * switched to an unified startOnboarding endpoint
    * added saving of tokens to localStorage
    * upgraded dependencies
    * added details on the new implementation diagram
    * switched to not send the token to the frontend
    * unified code style

--- src/main.js
+++ src/main.js
@@ -1,50 +1,52 @@
-import { fakeBackendStart, fakeBackendUrl, fakeBackendStatus, fakeBackendScore  } from './fake_backend';
+import {
+  fakeBackendStartOnboarding,
+  fakeBackendStatus,
+  fakeBackendScore,
+} from "./fake_backend";
 
 const createIframe = (url) => {
-  const app = document.getElementById('app');
+  const app = document.getElementById("app");
   app.innerHTML = "";
   // Dynamically create the iframe
-  const frame = document.createElement('iframe');
+  const frame = document.createElement("iframe");
   frame.src = url;
-  frame.class = 'onboarding-view'
-  frame.id = 'app-frame'
-  frame.width = '100%';
-  frame.height = '100%';
+  frame.className = "onboarding-view";
+  frame.id = "app-frame";
+  frame.width = "100%";
+  frame.height = "100%";
   frame.allowUserMedia = true;
-  frame.setAttribute('frameborder', '0');
-  frame.setAttribute('allow', 'geolocation; microphone; camera;');
+  frame.setAttribute("frameborder", "0");
+  frame.setAttribute("allow", "geolocation; microphone; camera;");
   app.appendChild(frame);
-}
+};
 
 async function app() {
-  const app = document.getElementById('app');
+  const app = document.getElementById("app");
   try {
-    const {token} = await fakeBackendStart();
-    const {url} = await fakeBackendUrl(token);
-  
-    createIframe(url);
-    
-    const interval = setInterval(async () => {
+    const { url, interviewId } = await fakeBackendStartOnboarding();
 
+    createIframe(url);
+
+    const interval = setInterval(async () => {
       try {
-        const {onboardingStatus} = await fakeBackendStatus(token);
-        if (onboardingStatus==='ONBOARDING_FINISHED'){
+        const { onboardingStatus } = await fakeBackendStatus(interviewId);
+        if (onboardingStatus === "ONBOARDING_FINISHED") {
           clearInterval(interval);
-          
+
           // Remove iframe from the parent node
-          const frame = document.getElementById('app-frame');
+          const frame = document.getElementById("app-frame");
           frame.parentNode.removeChild(frame);
-          const {overallStatus} = await fakeBackendScore(token);
-          app.innerHTML =`<h1>Onboarding finished with overall status: ${overallStatus}</h1>`;
+          const { overallStatus } = await fakeBackendScore(interviewId);
+          app.innerHTML = `<h1>Onboarding finished with overall status: ${overallStatus}</h1>`;
         }
-      } catch(e){
+      } catch (e) {
         clearInterval(interval);
-        app.innerHTML =`<h1>There was an error: ${e.message}</h1>`;
+        app.innerHTML = `<h1>There was an error: ${e.message}</h1>`;
       }
     }, 2000);
-  } catch(e) {
-    app.innerHTML =`<h1>There was an error: ${e.message}</h1>`;
-  } 
+  } catch (e) {
+    app.innerHTML = `<h1>There was an error: ${e.message}</h1>`;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", app);
